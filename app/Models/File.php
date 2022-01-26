6079bf488a4249b3fe236a68197445fc0a17fd80
hmimeee <hmimeee@gmail.com>
2022-01-26 19:13:33 +0600

    Updated bug fixing and files model

---
+++ app/Models/File.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class File extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'file_name',
+        'file_ext',
+        'file_size',
+        'path',
+        'description'
+    ];
+
+    /**
+     * Get the user that owns the File
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
