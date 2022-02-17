e73054ad2dee40e99b80a55201da02424e4d1b23
hmimeee <hmimeee@gmail.com>
2022-02-17 19:26:18 +0600

    Updated the activities

--- resources/lang/en/activities.php
+++ resources/lang/en/activities.php
@@ -14,9 +14,111 @@ return [
         'restored' => 'Restored the user account'
     ],
     'parts' => [
+        'self' => [
+            'created' => 'Added a part',
+            'updated' => 'Updated a part details',
+            'deleted' => 'Archived a part data',
+            'restored' => 'Restored a part data',
+        ],
         'created' => 'Added the part',
         'updated' => 'Updated the part details',
         'deleted' => 'Archived the part data',
         'restored' => 'Restored the part data',
-    ]
+    ],
+    'machines' => [
+        'self' => [
+            'created' => 'Added a new machine',
+            'updated' => 'Updated a machine details',
+            'deleted' => 'Archived a machine data',
+            'restored' => 'Restored a machine data',
+        ],
+        'created' => 'Added the machine',
+        'updated' => 'Updated the machine details',
+        'deleted' => 'Archived the machine data',
+        'restored' => 'Restored the machine data',
+    ],
+    'machine_models' => [
+        'self' => [
+            'created' => 'Added a new machine model',
+            'updated' => 'Updated a machine model details',
+            'deleted' => 'Archived a machine model data',
+            'restored' => 'Restored a machine model data',
+        ],
+        'created' => 'Added the machine model',
+        'updated' => 'Updated the machine model details',
+        'deleted' => 'Archived the machine model data',
+        'restored' => 'Restored the machine model data',
+    ],
+    'companies' => [
+        'self' => [
+            'created' => 'Added a new company',
+            'updated' => 'Updated a company details',
+            'deleted' => 'Archived a company data',
+            'restored' => 'Restored a company data',
+        ],
+        'created' => 'Added the company',
+        'updated' => 'Updated the company details',
+        'deleted' => 'Archived the company data',
+        'restored' => 'Restored the company data',
+    ],
+    'contracts' => [
+        'self' => [
+            'created' => 'Created a new contract',
+            'updated' => 'Updated a contract',
+            'deleted' => 'Archived a contract',
+            'restored' => 'Restored a contract',
+        ],
+        'created' => 'Created the contract',
+        'updated' => 'Updated the contract',
+        'deleted' => 'Archived the contract',
+        'restored' => 'Restored the contract',
+    ],
+    'designations' => [
+        'self' => [
+            'created' => 'Created a new designation',
+            'updated' => 'Updated a designation',
+            'deleted' => 'Archived a designation',
+            'restored' => 'Restored a designation',
+        ],
+        'created' => 'Created the designation',
+        'updated' => 'Updated the designation',
+        'deleted' => 'Archived the designation',
+        'restored' => 'Restored the designation',
+    ],
+    'roles' => [
+        'self' => [
+            'created' => 'Created a new role',
+            'updated' => 'Updated a role',
+            'deleted' => 'Archived a role',
+            'restored' => 'Restored a role',
+        ],
+        'created' => 'Created the role',
+        'updated' => 'Updated the role',
+        'deleted' => 'Archived the role',
+        'restored' => 'Restored the role',
+    ],
+    'roles' => [
+        'self' => [
+            'created' => 'Created a new role',
+            'updated' => 'Updated a role',
+            'deleted' => 'Archived a role',
+            'restored' => 'Restored a role',
+        ],
+        'created' => 'Created the role',
+        'updated' => 'Updated the role',
+        'deleted' => 'Archived the role',
+        'restored' => 'Restored the role',
+    ],
+    'warehouses' => [
+        'self' => [
+            'created' => 'Created a new warehouse',
+            'updated' => 'Updated a warehouse',
+            'deleted' => 'Archived a warehouse',
+            'restored' => 'Restored a warehouse',
+        ],
+        'created' => 'Created the warehouse',
+        'updated' => 'Updated the warehouse',
+        'deleted' => 'Archived the warehouse',
+        'restored' => 'Restored the warehouse',
+    ],
 ];
