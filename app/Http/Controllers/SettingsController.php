dd1da9a9bd82397cb89babd6632d9cc19889b295
unknown <shohorabshanto@gmail.com>
2022-05-31 18:47:05 +0600

    working with settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -26,24 +26,24 @@ class SettingsController extends Controller
     public function store(Request $request)
     {
 
-       return $request->file('icon')->getClientOriginalExtension();
+    //    return $request->file('icon')->getClientOriginalExtension();
 
-        if($request->hasFile('logo'))
-        {
-            $file = $request->file('logo');
-            $extention = $file->getClientOriginalExtension();
-            $fileName = time() .'.'.$extention;
-            $file->move('uploads/logo-icons/',$fileName);
+    //     if($request->hasFile('logo'))
+    //     {
+    //         $file = $request->file('logo');
+    //         $extention = $file->getClientOriginalExtension();
+    //         $fileName = time() .'.'.$extention;
+    //         $file->move('uploads/logo-icons/',$fileName);
 
-        }
-        if($request->hasFile('icon'))
-        {
-            $file = $request->file('icon');
-            $extention = $file->getClientOriginalExtension();
-            $fileName = time() .'.'.$extention;
-            $file->move('uploads/logo-icons/',$fileName);
+    //     }
+    //     if($request->hasFile('icon'))
+    //     {
+    //         $file = $request->file('icon');
+    //         $extention = $file->getClientOriginalExtension();
+    //         $fileName = time() .'.'.$extention;
+    //         $file->move('uploads/logo-icons/',$fileName);
 
-        }
+    //     }
 
         $data = collect($request->all())
         ->map(fn($value, $key) => [
