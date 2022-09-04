106281eabedb17986c16ba6283cf999a9d3d0957
Imrul Afnan <imrul.afnan18@gmail.com>
2022-09-04 18:02:09 +0600

    update recent sale

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,39 +18,28 @@ class DashboardController extends Controller
     public function sellPurchase()
     {
         $stocks = StockHistory::with('stock')->whereYear('created_at', Carbon::now()->year)->get();
-        $intotal = 0;
+        $buy = 0;
+        $sell = 0;
+        $profit = 0;
         foreach ($stocks as $key => $stock) {
             if ($stock->type == 'addition') {
                 $price = $stock->stock->yen_price;
                 $unit = $stock->current_unit_value - $stock->prev_unit_value;
                 $total = $unit * $price;
-                $intotal = $intotal + $total;
-            }
-        }
-        $buy = $intotal;
-
-        $intotal = 0;
-        foreach ($stocks as $key => $stock) {
-            if ($stock->type == 'deduction') {
+                $buy = $buy + $total;
+            } else {
                 $price = $stock->stock->selling_price;
                 $unit = $stock->prev_unit_value - $stock->current_unit_value;
                 $total = $unit * $price;
-                $intotal = $intotal + $total;
-            }
-        }
-        $sell = $intotal;
+                $sell = $sell + $total;
 
-        $intotal = 0;
-        foreach ($stocks as $key => $stock) {
-            if ($stock->type == 'deduction') {
                 $profit_per_unit = $stock->stock->selling_price - $stock->stock->yen_price;
                 $unit = $stock->prev_unit_value - $stock->current_unit_value;
                 $total = $unit * $profit_per_unit;
-                $intotal = $intotal + $total;
+                $profit = $profit + $total;
             }
         }
 
-        $profit = $intotal;
         return response()->json(['sell' => $sell, 'buy' => $buy, 'profit' => $profit]);
     }
 
@@ -78,7 +67,7 @@ class DashboardController extends Controller
 
     public function StockAlert()
     {
-        $stock = PartStock::with(['warehouse', 'part.aliases'])->where('unit_value', '<', 10)->whereYear('created_at', Carbon::now()->year)->take(5)->get();
+        $stock = PartStock::with(['warehouse', 'part.aliases'])->where('unit_value', '<', 10)->whereYear('created_at', Carbon::now()->year)->orderBy('updated_at', 'DESC')->take(5)->get();
         return PartStockAlertCollection::collection($stock);
     }
 
@@ -97,16 +86,17 @@ class DashboardController extends Controller
             ->join('companies', 'companies.id', '=', 'invoices.company_id')
             ->join('parts', 'parts.id', '=', 'part_items.part_id')
             ->join('part_aliases', 'part_aliases.part_id', '=', 'part_items.part_id')
-            ->select('part_items.id', 'part_items.created_at', 'part_items.quantity', 'part_aliases.name as part_name', 'part_aliases.part_number', 'companies.name as company_name','parts.id as part_id')->latest();
+            ->select('part_items.id', 'part_items.created_at', 'part_items.quantity', 'part_aliases.name as part_name', 'part_aliases.part_number', 'companies.name as company_name', 'parts.id as part_id')->latest();
 
-            // return DeliveryNote::collection($soldItems->get());
+        // return DeliveryNote::collection($soldItems->get());
 
-            // return $soldItems;
+        // return $soldItems;
 
         return RecentSaleCollection::collection($soldItems->get());
     }
 
-    public function TopCustomers(){
+    public function TopCustomers()
+    {
         $stocks = StockHistory::with('company')->whereYear('created_at', Carbon::now()->year)->where('type', 'deduction')->groupBy('company_id')->orderBy('created_at', 'ASC')->latest()->take(5)->get();
         foreach ($stocks as $key => $stock) {
             $stock->stock->part->aliases;
